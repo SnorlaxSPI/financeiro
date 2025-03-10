22a3841fdf03eabe8652956ae446d0c90b7ddc2c
Alessandro L <alessandro.lins@gruponewway.com.br>
2025-03-10 20:18:15 -0300

    Correção web.php

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FinanceiroController;
 
-Route::resource('financeiros', 'App\Http\Controllers\FinanceiroController');
+Route::redirect('/', '/financeiros');
+Route::resource('financeiros', FinanceiroController::class);
 Route::get('/financeiros/{id}/edit', [FinanceiroController::class, 'edit'])->name('financeiros.edit');
 Route::put('/financeiros/{id}', [FinanceiroController::class, 'update'])->name('financeiros.update');
 
