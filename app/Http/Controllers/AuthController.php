bac94301d8e3343f2a9bb148aa18da8153f5e023
SnorlaxSPI <aflinsjr@icloud.com>
2025-05-14 21:04:51 -0300

    fix financeiro

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,16 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
   public function login(Request $request)
   {
-    $credentials = $request->only('email', 'password');
+    // Valida os dados do formulário
+    $request->validate([
+      'email' => 'required|email',
+      'password' => 'required',
+    ]);
 
-    // Verifica se o email e senha estão corretos
-    if ($credentials['email'] === 'aflinsjr@gmail.com' && $credentials['password'] === '123mudar') {
-      return redirect('/financeiros'); // Redireciona para a tela do financeiro
+    // Tenta autenticar o usuário
+    if (Auth::attempt($request->only('email', 'password'))) {
+      // Redireciona para a tela do financeiro se as credenciais forem válidas
+      return redirect('/financeiros');
     }
 
     // Retorna erro se as credenciais forem inválidas
