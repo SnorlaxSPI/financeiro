d7093f8b57aeefc8b6a891d1b3ddeaeb7d3f2b3a
SnorlaxSPI <aflinsjr@icloud.com>
2025-05-14 21:12:03 -0300

    fix financeiro auth

--- routes/web.php
+++ routes/web.php
@@ -3,22 +3,28 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FinanceiroController;
 use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
-  return redirect('/login'); // Redireciona para a página de login
+  return redirect('/login');
 });
 
 Route::get('/login', function () {
   return view('auth.login');
+})->name('login');
+
+Route::post('/login', [AuthController::class, 'login']);
+
+// Rotas protegidas por middleware 'auth'
+Route::middleware('auth')->group(function () {
+  Route::resource('financeiros', FinanceiroController::class);
+  Route::get('/financeiros/{id}/edit', [FinanceiroController::class, 'edit'])->name('financeiros.edit');
+  Route::put('/financeiros/{id}', [FinanceiroController::class, 'update'])->name('financeiros.update');
+
+  Route::post('/logout', function () {
+    Auth::logout();
+    request()->session()->invalidate();
+    request()->session()->regenerateToken();
+    return redirect('/login');
+  })->name('logout');
 });
-
-Route::post('/login', [AuthController::class, 'login'])->name('login'); // Rota para autenticação
-
-Route::resource('financeiros', FinanceiroController::class);
-Route::get('/financeiros/{id}/edit', [FinanceiroController::class, 'edit'])->name('financeiros.edit');
-Route::put('/financeiros/{id}', [FinanceiroController::class, 'update'])->name('financeiros.update');
-
-Route::post('/logout', function () {
-  // Aqui você pode implementar a lógica de logout, como limpar a sessão
-  return redirect('/login');
-})->name('logout');
